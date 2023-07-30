b5528f529d0c017ab8cbbede97fd9087f583a605
unknown <jananirave19@gmail.com>
2023-07-30 11:21:34 +0530

    added edit action

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 const todo_display = document.getElementById("todo-container") 
 const newTodo = document.getElementById("new_todo")
 const delete_btn = document.querySelector(".delete")
+const allTodo = document.querySelectorAll(".todo")
 const btn = document.getElementById("add_new_todo")
 const enter_search = document.getElementById("search")
 const searchInput = document.getElementById("search_input")
-
 
 
 //Add new Task
@@ -17,24 +17,24 @@ btn.addEventListener("click",()=>{
         div.className = "todo";
         div.innerHTML = `<i class="far fa-square done-icon"></i>
                          <i class="far fa-check-square done-icon"></i>
-                         <span class="todo-text">${newTodo.value}</span>
-                        <i class="fa-solid fa-trash"></i>`
+                         <input class="todo-text" value=${newTodo.value} style="border: none" readonly/>
+                         <i class="edit">Edit</i/>
+                         <i class="fa-solid fa-trash"></i>`
                                        
        todo_display.appendChild(div); 
                    
         newTodo.value = ""                
-      }
+      }}
       
-   })
+   )
 
-//<i class="fa-solid fa-edit fa-md"></i>
 
 
 //delete a todo and complete a todo
 
-todo_display.addEventListener("click",removeTodo)
+todo_display.addEventListener("click",actions)
 
-function removeTodo(e) {
+function actions(e) {
    // Remove todo
    console.log("clicked")
    if (e.target.classList.contains('fa-trash')) {
@@ -43,29 +43,46 @@ function removeTodo(e) {
        }
    }
    //completed a todo
-    if (e.target.classList.contains('todo-text')) {
-       e.target.parentElement.classList.toggle('done');
-    }
     if (e.target.classList.contains('done-icon')) {
        e.target.parentElement.classList.toggle('done');
   }
+  //edit a todo
+  if(e.target.classList.contains("edit")){
+      if(e.target.innerText == "Edit"){
+        const sibling = e.target.previousElementSibling
+        sibling.removeAttribute("readonly");
+        sibling.focus();
+         e.target.innerText = "Save";
+         sibling.style.textDecoration="none"}
+     else{
+        e.target.previousElementSibling.setAttribute("readonly", "readonly");
+        e.target.innerText ="Edit";
+     }    
 }
+
+  }
+
 
 
 //search todo
 enter_search.addEventListener("click", searchTodo)
-   function searchTodo(e) {
+   function searchTodo() {
       const text = searchInput.value.toLowerCase();
-      const allItem = document.querySelectorAll('.todo');
-      for (let task of allItem) {
-          const item = task.textContent;
-          if (item.toLowerCase().indexOf(text) != -1) {
-              task.style.display = 'flex';
-          } else {
-              task.style.display = 'none';
+      const alltodoText = document.querySelectorAll('.todo-text');
+      const todos = document.querySelectorAll(".todo")
+      console.log(alltodoText)
+          for(let task of alltodoText){
+            const item = task.value
+            if(item.toLowerCase().indexOf(text) != -1){
+                task.parentElement.style.display = "flex"
+            }else{
+                task.parentElement.style.display = "none"
+            }
           }
+    
       };
-  }
+
+
 
  
  
